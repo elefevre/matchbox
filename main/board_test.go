a294869b08e945a778071f77679c6660fa7846ff
Eric Lefevre-Ardant <eric.lefevre-ardant@schibsted.com>
2019-06-13 07:31:03 +0200

    Replace row tests with a property-based test

--- main/board_test.go
+++ main/board_test.go
@@ -35,10 +35,6 @@ func TestBoard_can_add_coins_in_different_columns(t *testing.T) {
 	assertNumberOfCoins(t, 1, board, 1)
 }
 
-func Add(a, b int) int {
-	return a / b
-}
-
 func (PlayerColor) Generate(r *rand.Rand, size int) reflect.Value {
 	return reflect.ValueOf(PlayerColor(1 + r.Int31n(2)))
 }
@@ -46,17 +42,17 @@ func (PlayerColor) Generate(r *rand.Rand, size int) reflect.Value {
 type columnType byte
 
 func (columnType) Generate(r *rand.Rand, size int) reflect.Value {
-	return reflect.ValueOf(columnType(r.Int31n(7)))
+	return reflect.ValueOf(columnType(r.Int31n(int32(numberOfColumns))))
 }
 
-type coinsBelowType byte
+type coinsBelow4CoinsInAColumnType byte
 
-func (coinsBelowType) Generate(r *rand.Rand, size int) reflect.Value {
-	return reflect.ValueOf(coinsBelowType(r.Int31n(int32(numberOfRows - 4))))
+func (coinsBelow4CoinsInAColumnType) Generate(r *rand.Rand, size int) reflect.Value {
+	return reflect.ValueOf(coinsBelow4CoinsInAColumnType(r.Int31n(int32(numberOfRows - 4))))
 }
 
-func TestBoard_red_wins_when_there_are_4_red_consecutive_coins_in_a_column(t *testing.T) {
-	comm := func(column columnType, numberOfOpponentCoinsBelow coinsBelowType, player PlayerColor) bool {
+func TestBoard_player_wins_when_there_are_4_consecutive_coins_in_a_column(t *testing.T) {
+	comm := func(column columnType, numberOfOpponentCoinsBelow coinsBelow4CoinsInAColumnType, player PlayerColor) bool {
 		opponent := yellow
 		if player == yellow {
 			opponent = red
@@ -90,23 +86,54 @@ func TestBoard_red_wins_when_there_are_4_red_consecutive_coins_in_a_column(t *te
 	}
 }
 
-func TestBoard_red_wins_when_she_plays_4_coins_in_the_second_row_starting_from_first_column(t *testing.T) {
-	board := &Board{}
+type startColumnFor4CoinsInARowType byte
 
-	board.InsertCoin(red, 0)
-	board.InsertCoin(yellow, 1)
-	board.InsertCoin(yellow, 2)
-	board.InsertCoin(red, 3)
+func (startColumnFor4CoinsInARowType) Generate(r *rand.Rand, size int) reflect.Value {
+	return reflect.ValueOf(startColumnFor4CoinsInARowType(r.Int31n(int32(numberOfColumns - 4))))
+}
 
-	board.InsertCoin(red, 0)
-	assertNotWin(t, board, red)
-	board.InsertCoin(red, 1)
-	assertNotWin(t, board, red)
-	board.InsertCoin(red, 2)
-	assertNotWin(t, board, red)
-	board.InsertCoin(red, 3)
+type coinsBelow4CoinsInARowType byte
 
-	assertWin(t, board, red)
+func (coinsBelow4CoinsInARowType) Generate(r *rand.Rand, size int) reflect.Value {
+	return reflect.ValueOf(coinsBelow4CoinsInARowType(r.Int31n(int32(numberOfRows - 1))))
+}
+
+func TestBoard_player_wins_when_there_are_4_consecutive_coins_in_a_row(t *testing.T) {
+	comm := func(column startColumnFor4CoinsInARowType, numberOfOpponentCoinsBelow coinsBelow4CoinsInARowType, player PlayerColor) bool {
+		opponent := yellow
+		if player == yellow {
+			opponent = red
+		}
+
+		board := &Board{}
+
+		for i := byte(0); i < byte(numberOfOpponentCoinsBelow); i++ {
+			board.InsertCoin(opponent, byte(column))
+			board.InsertCoin(opponent, byte(column+1))
+			board.InsertCoin(opponent, byte(column+2))
+			board.InsertCoin(opponent, byte(column+3))
+		}
+
+		board.InsertCoin(player, byte(column))
+		if board.HasWon(player) {
+			return false
+		}
+		board.InsertCoin(player, byte(column+1))
+		if board.HasWon(player) {
+			return false
+		}
+		board.InsertCoin(player, byte(column+2))
+		if board.HasWon(player) {
+			return false
+		}
+		board.InsertCoin(player, byte(column+3))
+
+		return board.HasWon(player)
+	}
+
+	if err := quick.Check(comm, nil); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestBoard_red_wins_when_she_plays_4_coins_in_a_diagonal_from_left_bottom_corner(t *testing.T) {
@@ -145,32 +172,6 @@ func TestBoard_red_wins_when_she_plays_4_coins_in_a_diagonal_from_left_bottom_co
 	board.InsertCoin(red, 1)
 	assertNotWin(t, board, red)
 	board.InsertCoin(red, 0)
-
-	assertWin(t, board, red)
-}
-
-func TestBoard_red_wins_when_she_plays_4_coins_in_the_second_row_starting_from_first_column_on_2nd_row(t *testing.T) {
-	board := &Board{}
-
-	board.InsertCoin(red, 0)
-	board.InsertCoin(yellow, 1)
-	board.InsertCoin(yellow, 2)
-	board.InsertCoin(red, 3)
-
-	board.InsertCoin(red, 0)
-	assertNotWin(t, board, red)
-	board.InsertCoin(yellow, 1)
-	board.InsertCoin(red, 1)
-	assertNotWin(t, board, red)
-	board.InsertCoin(yellow, 2)
-	board.InsertCoin(yellow, 2)
-	board.InsertCoin(red, 2)
-	assertNotWin(t, board, red)
-	board.InsertCoin(yellow, 3)
-	board.InsertCoin(yellow, 3)
-	board.InsertCoin(yellow, 3)
-	assertNotWin(t, board, red)
-	board.InsertCoin(red, 3)
 
 	assertWin(t, board, red)
 }
